3d2d8494de6ae87d048d5d6fb67175b4a5abe439
Kim DongMin <vloeiolzlr@gmail.com>
2025-09-06 17:31:43 +0900

    build: Initial Commit

---
+++ src/main/java/com/web/coreclass/CoreclassApplication.java
@@ -0,0 +1,13 @@
+package com.web.coreclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreclassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreclassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/web/coreclass/CoreclassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.coreclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreclassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
